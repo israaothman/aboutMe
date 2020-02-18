2e225dc3c93114e5b0d8614428dc2ef998fcf42b
israaothman <israaayoub@hotmail.com>
2020-02-18 10:29:40 +0200

    eslint

--- js/app.js
+++ js/app.js
@@ -1,70 +1,70 @@
-'use strict'
+'use strict';
 
-var name = prompt("Hello my name is Israa , Whats your name ?");
- alert("Welcome to my website " + name  + " :) ");
+var Uname = prompt('Hello my name is Israa , Whats your name ?');
+alert('Welcome to my website ' + Uname + ' :)');
 
 /*confirm(" Are you a female ? ");
 if(confirm == true){
-    document.body.style.background = "pink"; 
+    document.body.style.background = "pink";
 } else {
-    document.body.style.background = "lightblue"; 
+    document.body.style.background = "lightblue";
 }*/
 
 
-var gender = prompt("Are you a female ? please let your answer be yes or no").toUpperCase();
-if(gender == 'YES' || gender == 'Y'){
-    document.body.style.background = "pink";
-    alert(" the background of the webpage will be pink "); 
-} else if(gender == 'NO' || gender == 'N') {
-    document.body.style.background = "lightblue"; 
-    alert(" the background of the webpage will be lightblue "); 
+var gender = prompt('Are you a female ? please let your answer be yes or no').toUpperCase();
+if(gender === 'YES' || gender === 'Y'){
+  document.body.style.background = 'pink';
+  alert('the background of the webpage will be pink');
+} else if(gender === 'NO' || gender === 'N') {
+  document.body.style.background = 'lightblue';
+  alert(' the background of the webpage will be lightblue ');
 } else {
-    alert(" your didn't enter yes or no  :(  ")
+  alert(' your didn\'t enter yes or no  :( ');
 }
 
-var me = prompt("Do you know if I am a girl or a boy ?").toUpperCase();
-if(me == 'YES' || me == 'Y'){
-    alert(" Yes I am a girl ");
-} else if(me == 'NO' || me == 'N') {
-    alert(" its ok .. I am a girl");
+var me = prompt('Do you know if I am a girl or a boy ?').toUpperCase();
+if(me === 'YES' || me === 'Y'){
+  alert(' Yes I am a girl ');
+} else if(me === 'NO' || me === 'N') {
+  alert(' its ok .. I am a girl');
 } else {
-    alert(" your didn't enter yse or no :( ...  I am Girl ")
-}
-  
-var prog = prompt("Do you think that I like programming ? ").toUpperCase();
-if(prog == 'YES' || prog == 'Y'){
-    alert(" Yes I like it so much ");
-} else if(prog == 'NO' || prog == 'N') {
-    alert(" No I do like it  ");
-} else {
-    alert(" your didn't enter yes or no :(  ... yes I like it ")
-
-var handmade = prompt("Do you think that I like the hand made things ? ").toUpperCase();
-if(handmade == 'YES' || handmade == 'Y'){
-    alert(" Yes I like it and I have my own hand made gifts");
-} else if(handmade == 'NO' || handmade == 'N') {
-    alert("No , I do like it and I have my own hand made gifts ");
-} else {
-    alert(" your didn't enter yes or no :( ... yes I do like it ")
-}
+  alert(' your didn\'t enter yse or no :( ...  I am Girl');
 }
 
-var art = prompt("Do you know what kind of hand made I use to make ? ").toUpperCase();
-if(art == 'YES' || art == 'Y'){
-    alert(" So you already know me , thats great ");
+var prog = prompt('Do you think that I like programming ? ').toUpperCase();
+if(prog === 'YES' || prog === 'Y'){
+  alert(' Yes I like it so much ');
+} else if(prog === 'NO' || prog === 'N') {
+  alert(' No I do like it ');
+} else {
+  alert('your didn\'t enter yes or no :(  ... yes I like it ');
+
+  var handmade = prompt('Do you think that I like the hand made things ? ').toUpperCase();
+  if(handmade === 'YES' || handmade === 'Y'){
+    alert(' Yes I like it and I have my own hand made gifts');
+} else if(handmade === 'NO' || handmade === 'N') {
+    alert('No , I do like it and I have my own hand made gifts ');
+} else {
+    alert(' your didn\'t enter yes or no :( ... yes I do like it ');
+}
+}
+
+var art = prompt('Do you know what kind of hand made I use to make ? ').toUpperCase();
+if(art === 'YES' || art === 'Y'){
+  alert(' So you already know me , thats great ');
 } else if(art == 'NO' || art == 'N') {
-    alert(" The hand made that I use to make is String art ");
+  alert(' The hand made that I use to make is String art ');
 } else {
-    alert(" your didn't enter yes or no :( ... I make a string art ")
+  alert(' your didn\'t enter yes or no :( ... I make a string art ');
 }
 
-var art = prompt("Do you know what the String art is ? ").toUpperCase();
-if(art == 'YES' || art == 'Y'){
-    alert(" Great , I'm gonna show you some of my works ");
-} else if(art == 'NO' || art == 'N') {
-    alert(" It's ok .. now your going to know it  ");
+var str = prompt('Do you know what the String art is ? ').toUpperCase();
+if(str === 'YES' || str === 'Y'){
+  alert(' Great , I\'m gonna show you some of my works ');
+} else if(str === 'NO' || str === 'N') {
+  alert(' It\'s ok .. now your going to know it ');
 } else {
-    alert(" your didn't enter yes or no :( ")
+  alert('your didn\'t enter yes or no :( ');
 }
 
 
@@ -73,10 +73,10 @@ if(art == 'YES' || art == 'Y'){
     var type = typeof name;
     console.log(type);
 
-    if(type === String ){        
+    if(type === String ){
     var msg = alert("Welcome to my website " + name  + " :) ");
     } else {
-        msg = Name(name) ;   
+        msg = Name(name) ;
     }
  return msg ;
 }*/
